d5547dbaa4b6dbc9c97264423fa0d543d300af04
matheus-carretta <matheuscarretta@gmail.com>
2023-04-14 22:39:46 -0300

    feat: create user interface and dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { User } from '../entities/user.entity';
+import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
+
+export class CreateUserDto extends User {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+}
