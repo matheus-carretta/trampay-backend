688dddd80af1bc85af4c5895167c5a4ee13ae81a
matheus-carretta <matheuscarretta@gmail.com>
2023-04-16 22:55:42 -0300

    feat: add recoverToken field and method to insert

---
+++ src/user/dto/change-password.dto.ts
@@ -0,0 +1,10 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
+export class ChangePasswordDto {
+  @IsString()
+  @MinLength(8)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+}
