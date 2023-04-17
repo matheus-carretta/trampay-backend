6bcbf71dc2340c54c773c6b7fbcb1b93b5951338
matheus-carretta <matheuscarretta@gmail.com>
2023-04-16 22:32:28 -0300

    feat: implements send recover email

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 import * as cors from 'cors';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.office365.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.SMTP_EMAIL,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      defaults: {
+        from: '"No Reply" <no-reply@example.com>',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [UserModule],
