adc917640841adbbac228db33caafde886cdeebb
matheus-carretta <matheuscarretta@gmail.com>
2023-04-18 19:42:55 -0300

    feat: implements createBalance from a csv file

--- src/balance/balance.service.ts
+++ src/balance/balance.service.ts
@@ -1,21 +1,57 @@
 import { Injectable } from '@nestjs/common';
-import { parse } from 'papaparse';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { Readable } from 'stream';
+import * as csvtojson from 'csvtojson';
+import { User } from 'src/user/entities/user.entity';
+import { Balance } from '@prisma/client';
 
+const today = new Date();
+today.setHours(0, 0, 0, 0);
 @Injectable()
 export class BalanceService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createBalances(fileBufferInBase64: string): Promise<void> {
-    const buffer = Buffer.from(fileBufferInBase64, 'base64');
-    const dataStream = Readable.from(buffer);
-    const parsedCsv = parse(dataStream, {
-      header: true,
-      skipEmptyLines: true,
-      complete: (results) => {
-        console.log('results:', results);
+  async findTodayBalances(user: User) {
+    const balances = await this.prisma.balance.findMany({
+      where: {
+        userId: user.id,
+        createdAt: {
+          gte: today,
+        },
       },
+    });
+
+    return balances;
+  }
+
+  async removeBalanceLogically(balances: Array<Balance>): Promise<void> {
+    await this.prisma.balance.updateMany({
+      data: { deletedAt: today },
+      where: { id: { in: balances.map((balance) => balance.id) } },
+    });
+  }
+
+  async createBalances(csv: string, user: User): Promise<void> {
+    const balanceArray = await csvtojson({
+      delimiter: ';',
+    }).fromString(csv);
+
+    const balancesToday = await this.findTodayBalances(user);
+
+    const balancesFound = balancesToday.length !== 0;
+
+    if (balancesFound) {
+      await this.removeBalanceLogically(balancesToday);
+    }
+
+    const balances = balanceArray.map((row) => ({
+      document: row.documento_recebedor,
+      balance: parseFloat(row.valor.replace(',', '.')),
+      date: row.data_do_lancamento_financeiro,
+      userId: user.id,
+    }));
+
+    await this.prisma.balance.createMany({
+      data: balances,
     });
   }
 }
