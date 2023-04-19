ad9c92db4d85e9c54f0735a5d945b5a64e63aef2
matheus-carretta <matheuscarretta@gmail.com>
2023-04-19 11:50:29 -0300

    fix: lint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -28,7 +28,8 @@ export class AuthController {
   ): Promise<{ message: string }> {
     await this.authService.sendRecoverPasswordEmail(email);
     return {
-      message: 'Foi enviado um email com instruções para resetar sua senha. Verifique o Spam.',
+      message:
+        'Foi enviado um email com instruções para resetar sua senha. Verifique o Spam.',
     };
   }
 }
